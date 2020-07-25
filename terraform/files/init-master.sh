ab4b756d336571b14b08a9c41fcc0df4450060d4
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2020-07-25 12:03:26 +0100

    logging niceties

--- terraform/files/init-master.sh
+++ terraform/files/init-master.sh
@@ -5,11 +5,12 @@ echo "********************************** INITIALISE KUBEADM ********************
 
 kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=10.0.0.137
 
-echo "about to mkdir"
-
+echo "setup kubeconfig for ubuntu user"
 mkdir -p /home/ubuntu/.kube
 cp -f /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
 chown $(id -u):$(id -g) /home/ubuntu/.kube/config
 chmod -R 755 /home/ubuntu/.kube
+
+echo "create kubectl alias"
 cp /usr/bin/kubectl /usr/bin/k
 
